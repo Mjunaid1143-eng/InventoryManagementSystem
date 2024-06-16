ea868b0e365a0c4b9ee07aa6bef9e1c208869187
junaid iqbal <junaidiqbal1143@hotmail.com>
2024-06-16 12:29:54 +0500

    Authentication

---
+++ WebUI/Components/Layout/Identity/SignOutEndpoint.cs
@@ -0,0 +1,23 @@
+﻿using Application.Extension.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace WebUI.Components.Layout.Identity
+{
+    internal static class SignOutEndpoint
+    {
+        public static IEndpointConventionBuilder MapSignOutEndpoint(this IEndpointRouteBuilder endpoint)
+        {
+            ArgumentNullException.ThrowIfNull(endpoint);
+            var accountGroup = endpoint.MapGroup("/Account");
+            accountGroup.MapPost("/Logout", async (ClaimsPrincipal user, SignInManager<ApplicationUser> SignInManager) =>
+            {
+                await SignInManager.SignOutAsync();
+                return TypedResults.LocalRedirect("/Account/Login");
+            });
+
+            return accountGroup;
+        }
+    }
+}
